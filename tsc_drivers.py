506d4c3a86f6cd80d90c531dd07eac7dd6500a65
jterry64 <justin.terry@wri.org>
2020-08-03 10:43:22 -0400

    Update code to latest versions

--- tsc_drivers.py
+++ tsc_drivers.py
@@ -6,9 +6,9 @@ gee.init()
 #
 # DATA
 #
-PROJECT_ROOT='projects/wri-datalab/TSC_DRIVERS/2017'
+PROJECT_ROOT='projects/wri-datalab/TSC_DRIVERS/2019'
 TSC_DRIVERS_ID=PROJECT_ROOT+'/lossyear_classification'
-HANSEN_THRESHOLDED_LOSS_ID='projects/wri-datalab/umd/HANSEN_BINARY_LOSS_17'
+HANSEN_THRESHOLDED_LOSS_ID='projects/wri-datalab/umd/HANSEN_BINARY_LOSS_19'
 
 
 #
@@ -27,7 +27,7 @@ Z_LEVELS=[156000,78000,39000,20000,10000,4900,2400,1200,611,305,152,76,38,19]
 MAX_PIXS=65500
 FULL_INTENSITY=255
 BANDS=['intensity','class','lossyear']
-GCE_TILE_ROOT='lossyear_classification_map/2017/gfw'
+# GCE_TILE_ROOT='v2020'
 THRESHOLDS=[10,15,20,25,30,50,75]
 DEFAULT_GEOM_NAME='hansen_world'
 DEFAULT_VERSION=2
@@ -37,14 +37,72 @@ Z_MAX=12
 
 
 
+
 #
 # GEOMETRY
 #
 geom=None
 geom_name=None
-geoms_ft=ee.FeatureCollection('ft:13BvM9v1Rzr90Ykf1bzPgbYvbb8kGSvwyqyDwO8NI')
-def get_geom(name):
-    return ee.Feature(geoms_ft.filter(ee.Filter.eq('name',name)).first()).geometry()
+#geoms_ft=ee.FeatureCollection('ft:13BvM9v1Rzr90Ykf1bzPgbYvbb8kGSvwyqyDwO8NI')
+def get_geom():
+    return ee.Geometry.MultiPolygon([[[[180.0, -50.0], [180.0, -60.0], [170.0, -60.0], [160.0, -60.0], [150.0, -60.0],
+                                       [150.0, -50.0], [140.0, -50.0], [140.0, -40.0], [130.0, -40.0], [120.0, -40.0],
+                                       [110.0, -40.0], [110.0, -30.0], [110.0, -20.0], [100.0, -20.0], [90.0, -20.0],
+                                       [90.0, -10.0], [90.0, 0.0], [80.0, 0.0], [80.0, -10.0], [70.0, -10.0],
+                                       [70.0, -20.0], [60.0, -20.0], [60.0, -30.0], [50.0, -30.0], [40.0, -30.0],
+                                       [40.0, -40.0], [30.0, -40.0], [20.0, -40.0], [10.0, -40.0], [10.0, -30.0],
+                                       [10.0, -20.0], [10.0, -10.0], [0.0, -10.0], [0.0, 0.0], [-10.0, 0.0],
+                                       [-10.0, -10.0], [-20.0, -10.0], [-20.0, 0.0], [-20.0, 10.0], [-30.0, 10.0],
+                                       [-30.0, 20.0], [-20.0, 20.0], [-20.0, 30.0], [-30.0, 30.0], [-40.0, 30.0],
+                                       [-40.0, 40.0], [-30.0, 40.0], [-20.0, 40.0], [-10.0, 40.0], [-10.0, 50.0],
+                                       [-20.0, 50.0], [-20.0, 60.0], [-30.0, 60.0], [-40.0, 60.0], [-40.0, 50.0],
+                                       [-50.0, 50.0], [-50.0, 40.0], [-60.0, 40.0], [-60.0, 30.0], [-70.0, 30.0],
+                                       [-70.0, 20.0], [-60.0, 20.0], [-50.0, 20.0], [-50.0, 10.0], [-40.0, 10.0],
+                                       [-40.0, 0.0], [-30.0, 0.0], [-30.0, -10.0], [-30.0, -20.0], [-40.0, -20.0],
+                                       [-40.0, -30.0], [-50.0, -30.0], [-50.0, -40.0], [-60.0, -40.0], [-60.0, -50.0],
+                                       [-50.0, -50.0], [-40.0, -50.0], [-30.0, -50.0], [-20.0, -50.0], [-20.0, -60.0],
+                                       [-30.0, -60.0], [-40.0, -60.0], [-50.0, -60.0], [-60.0, -60.0], [-70.0, -60.0],
+                                       [-80.0, -60.0], [-80.0, -50.0], [-80.0, -40.0], [-90.0, -40.0], [-90.0, -30.0],
+                                       [-90.0, -20.0], [-80.0, -20.0], [-80.0, -10.0], [-90.0, -10.0], [-100.0, -10.0],
+                                       [-100.0, 0.0], [-100.0, 10.0], [-110.0, 10.0], [-120.0, 10.0], [-120.0, 20.0],
+                                       [-120.0, 30.0], [-130.0, 30.0], [-130.0, 40.0], [-130.0, 50.0], [-140.0, 50.0],
+                                       [-150.0, 50.0], [-160.0, 50.0], [-170.0, 50.0], [-180.0, 50.0], [-180.0, 60.0],
+                                       [-180.0, 70.0], [-180.0, 80.0], [-170.0, 80.0], [-160.0, 80.0], [-150.0, 80.0],
+                                       [-140.0, 80.0], [-130.0, 80.0], [-120.0, 80.0], [-110.0, 80.0], [-100.0, 80.0],
+                                       [-90.0, 80.0], [-80.0, 80.0], [-70.0, 80.0], [-60.0, 80.0], [-50.0, 80.0],
+                                       [-40.0, 80.0], [-30.0, 80.0], [-20.0, 80.0], [-10.0, 80.0], [0.0, 80.0],
+                                       [0.0, 70.0], [10.0, 70.0], [10.0, 80.0], [20.0, 80.0], [30.0, 80.0],
+                                       [40.0, 80.0], [50.0, 80.0], [60.0, 80.0], [70.0, 80.0], [80.0, 80.0],
+                                       [90.0, 80.0], [100.0, 80.0], [110.0, 80.0], [120.0, 80.0], [130.0, 80.0],
+                                       [140.0, 80.0], [150.0, 80.0], [160.0, 80.0], [170.0, 80.0], [180.0, 80.0],
+                                       [180.0, 70.0], [180.0, 60.0], [180.0, 50.0], [170.0, 50.0], [160.0, 50.0],
+                                       [160.0, 40.0], [150.0, 40.0], [150.0, 30.0], [160.0, 30.0], [160.0, 20.0],
+                                       [170.0, 20.0], [180.0, 20.0], [180.0, 10.0], [180.0, 0.0], [180.0, -10.0],
+                                       [180.0, -20.0], [180.0, -30.0], [180.0, -40.0], [180.0, -50.0]],
+                                      [[150.0, -50.0], [160.0, -50.0], [160.0, -40.0], [150.0, -40.0], [150.0, -50.0]],
+                                      [[160.0, -40.0], [170.0, -40.0], [170.0, -30.0], [160.0, -30.0], [160.0, -40.0]],
+                                      [[70.0, -10.0], [70.0, 0.0], [70.0, 10.0], [70.0, 20.0], [60.0, 20.0],
+                                       [60.0, 10.0], [60.0, 0.0], [60.0, -10.0], [70.0, -10.0]],
+                                      [[160.0, 20.0], [150.0, 20.0], [150.0, 10.0], [160.0, 10.0], [160.0, 20.0]]],
+                                     [[[0.0, -50.0], [10.0, -50.0], [10.0, -60.0], [0.0, -60.0], [0.0, -50.0]]], [
+                                         [[80.0, -40.0], [80.0, -50.0], [80.0, -60.0], [70.0, -60.0], [60.0, -60.0],
+                                          [60.0, -50.0], [50.0, -50.0], [50.0, -40.0], [60.0, -40.0], [70.0, -40.0],
+                                          [70.0, -30.0], [80.0, -30.0], [80.0, -40.0]]], [
+                                         [[-10.0, -40.0], [0.0, -40.0], [0.0, -50.0], [-10.0, -50.0], [-20.0, -50.0],
+                                          [-20.0, -40.0], [-20.0, -30.0], [-10.0, -30.0], [-10.0, -40.0]]], [
+                                         [[-110.0, -20.0], [-100.0, -20.0], [-100.0, -30.0], [-110.0, -30.0],
+                                          [-110.0, -20.0]]],
+                                     [[[-30.0, -20.0], [-20.0, -20.0], [-20.0, -30.0], [-30.0, -30.0], [-30.0, -20.0]]],
+                                     [[[-10.0, -10.0], [0.0, -10.0], [0.0, -20.0], [-10.0, -20.0], [-10.0, -10.0]]], [
+                                         [[-170.0, -40.0], [-170.0, -50.0], [-180.0, -50.0], [-180.0, -40.0],
+                                          [-180.0, -30.0], [-180.0, -20.0], [-180.0, -10.0], [-180.0, 0.0],
+                                          [-180.0, 10.0], [-170.0, 10.0], [-170.0, 20.0], [-180.0, 20.0],
+                                          [-180.0, 30.0], [-170.0, 30.0], [-160.0, 30.0], [-150.0, 30.0],
+                                          [-150.0, 20.0], [-150.0, 10.0], [-150.0, 0.0], [-140.0, 0.0], [-130.0, 0.0],
+                                          [-130.0, -10.0], [-130.0, -20.0], [-120.0, -20.0], [-120.0, -30.0],
+                                          [-130.0, -30.0], [-140.0, -30.0], [-150.0, -30.0], [-160.0, -30.0],
+                                          [-160.0, -20.0], [-170.0, -20.0], [-170.0, -30.0], [-170.0, -40.0]]]], None,
+                                    False)
 
 
 #
@@ -94,16 +152,16 @@ def export_tiles(image,z,v,threshold):
             fileFormat='png',
             image=image,
             description='{}__{}'.format(name,z), 
-            bucket='wri-public', 
+            bucket='tsc_tree_cover_loss_drivers',
             path=tiles_path, 
             writePublicTiles=False, 
             maxZoom=z, 
             minZoom=z, 
-            region=geom.coordinates().getInfo(), 
+            region=geom.bounds(),
             skipEmptyTiles=True
         )
         task.start()
-        if NOISY: print task.status()
+        if NOISY: print(task.status())
         return task
 
 
@@ -117,11 +175,11 @@ def export_asset(image,z,v,threshold):
             assetId='{}/{}'.format(PROJECT_ROOT,name), 
             scale=Z_LEVELS[z], 
             crs=CRS, 
-            region=geom.coordinates().getInfo(),
+            region=geom.bounds(),
             maxPixels=500000000
         )
         task.start()
-        if NOISY: print task.status()
+        if NOISY: print(task.status())
         return task
 
 
@@ -136,9 +194,9 @@ def run(img_i,img_cat,img_ly,maxz,minz,v,threshold,scale=SCALE,lowest_to_asset='
         zimg=zjoin(zimg_i,zimg_cat,zimg_ly)
         if z==minz:
             if (not lowest_to_asset) or (isinstance(lowest_to_asset,str) and lowest_to_asset.lower()=='false'):
-                print 'skiping inside-asset-export'
+                print('skiping inside-asset-export')
             else:
-                print 'export asset:',z
+                print('export asset:',z)
                 task=export_asset(zimg,z,v,threshold)
         task=export_tiles(zimg,z,v,threshold)
 
@@ -148,7 +206,7 @@ def run_zasset(img_i,img_cat,img_ly,z,v,threshold,scale=SCALE):
     zimg_cat=zmode(img_cat,z,scale)
     zimg_ly=zmode(img_ly,z,scale)
     zimg=zjoin(zimg_i,zimg_cat,zimg_ly)
-    print 'export asset:',z
+    print('export asset:',z)
     task=export_tiles(zimg,z,v,threshold)
 
 
@@ -156,7 +214,7 @@ def run_zasset(img_i,img_cat,img_ly,z,v,threshold,scale=SCALE):
 # PATH/IMG/IC HELPERS
 #
 def gce_tiles_path(v,threshold):
-    return '{}/tiles/{}/v{}/tc{}'.format(GCE_TILE_ROOT,geom_name,v,threshold)
+    return 'v{}/tcd_{}'.format(v,threshold)
 
 
 def zlevel_asset_name(v,z,threshold):
@@ -218,10 +276,10 @@ def main():
     args=parser.parse_args()
     if int(args.threshold) in THRESHOLDS: 
         geom_name=args.geom_name
-        geom=get_geom(geom_name)
+        geom=get_geom()
         args.func(args)
     else: 
-        print 'INVALID THRESHOLD:',args.threshold,args
+        print('INVALID THRESHOLD:',args.threshold,args)
 
 
 if __name__ == "__main__":
